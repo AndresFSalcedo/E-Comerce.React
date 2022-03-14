ec8b888e4d81ff7b75a2fc1ac6c02099ea7a05f7
AndresFSalcedo <84711845+AndresFSalcedo@users.noreply.github.com>
2022-03-15 12:05:25 +1300

    Update process to the newest version
    From Scratch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React from 'react';
-import './App.css';
 
-import HomePage from './pages/homepage/homepage.component';
-
-function App() {
+const App = () => {
   return (
-    <div>
-      <HomePage />
+    <div className='categories-container'>
+      <div className='category-container'>
+        {/*<img />*/}
+        <div className='category-body-container'>
+          <h2>Hats</h2>
+          <p>SHOP NOW</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/*<img />*/}
+        <div className='category-body-container'>
+          <h2>Jackets</h2>
+          <p>SHOP NOW</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/*<img />*/}
+        <div className='category-body-container'>
+          <h2>Sneakers</h2>
+          <p>SHOP NOW</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/*<img />*/}
+        <div className='category-body-container'>
+          <h2>Womens</h2>
+          <p>SHOP NOW</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/*<img />*/}
+        <div className='category-body-container'>
+          <h2>Mens</h2>
+          <p>SHOP NOW</p>
+        </div>
+      </div>
     </div>
   );
 }
